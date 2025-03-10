b79148c2a6683964f5a61e4f75cf4008ffc02f34
Kasperdejong <kasperdejong@gmail.com>
2025-03-10 14:10:44 +0100

    Updated homepage before Carousel

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,73 +1,93 @@
 function Home() {
     return (
-        <div className="flex flex-col items-center justify-center px-4 text-center">
-            <h2 className="text-4xl font-k2d">
-                Hallo {/*{users.name}*/} Abigail
-            </h2>
-            <p className="pb-4 mt-4 text-lg max-w-md font-openSans">
-                Welkom op SignWitch. Hier kan je extra oefeningen doen rondom gebarentaal.
-                Hier zie je een overzicht van je lessen, maar je kan ook het woordenboek bekijken en
-                overzicht van de lessen in de navigatiebalk.
-            </p>
+        <>
+            <div className="flex flex-col items-center justify-center px-4 text-center">
+                <h2 className="pt-16 text-6xl font-k2d">
+                    Hallo {/*{users.name}*/} Abigail
+                </h2>
+                <p className="pb-20 mt-4 text-2xl max-w-2xl font-openSans">
+                    Welkom op SignWitch. Hier kan je extra oefeningen doen rondom gebarentaal.
+                    Hier zie je een overzicht van je lessen, maar je kan ook het woordenboek bekijken en
+                    overzicht van de lessen in de navigatiebalk.
+                </p>
+            </div>
 
-            <div className="">
+            <div className="flex gap-20 flex-row items-center justify-center">
+                {/* Invisible Placeholder (for centering) */}
+                <div className="size-80 opacity-0"></div>
+                <div className="pb-36 text-4xl opacity-0">←</div>
+
+
                 <div className=" inset-0 flex flex-col items-center justify-center">
-                    <div className="w-60 h-60 rounded-full border border-black flex items-center justify-center relative">
-                        <div className="w-48 h-48 rounded-full border border-black"></div>
-                        <div className="absolute text-6xl text-black font-k2d">0%</div>
+                    <div className="relative size-80">
+                        <svg className="size-full -rotate-90" viewBox="0 0 36 36"
+                             xmlns="http://www.w3.org/2000/svg">
+                            <circle cx="18" cy="18" r="16" fill="none" className="stroke-current text-gray-200"
+                                    strokeWidth="2"></circle>
+                            <circle cx="18" cy="18" r="16" fill="none" className="stroke-current text-red-600"
+                                    strokeWidth="2" strokeDasharray="100" strokeDashoffset={100 - 35}
+                                    strokeLinecap="round"></circle>
+                        </svg>
+
+                        <div className="absolute top-1/2 start-1/2 transform -translate-y-1/2 -translate-x-1/2">
+                            <span className="text-center text-4xl font-bold text-black">35%</span>
+                        </div>
                     </div>
                     <div className="mt-4">
-                        <h2 className="text-2xl p-4 font-k2d">Les 1</h2>
+                        <h2 className="text-4xl p-4 font-k2d">Les 1</h2>
+                    </div>
+                        <div className="inline-block bg-red-600 text-white font-k2d text-xl py-2 px-24 rounded-md hover:bg-red-800 hover:text-white transition-colors"
+                        >
+                        Beginnen
+                        </div>
                         {/*
                             <Link
-                              to="/lessen/les1"
+                              to="/lessons/lesson1"
                               className="inline-block bg-purple-500 text-white font-semibold py-2 px-6 rounded-md hover:bg-blue-600 hover:text-white transition-colors"
                             >
                               Beginnen
                             </Link>
                             */}
-                    </div>
-                </div>
-                <div
-                    className="absolute"
-                    style={{
-                        left: "calc(50% + 200px)",
-                        top: "50%",
-                        transform: "translateY(-50%)",
-                    }}
-                >
-                    <div className="flex flex-col items-center">
-                        <div className="w-48 h-48 rounded-full border border-black flex items-center justify-center relative">
-                            <div className="w-36 h-36 rounded-full border border-black"></div>
-                            <div className="absolute text-4xl text-black font-k2d">0%</div>
-                        </div>
-                        <div className="mt-4 text-center">
-                            <h2 className="text-2xl font-k2d">Les 2</h2>
-                            {/*
-                              <Link
-                                to="/lessen/les2"
-                                className="inline-block bg-purple-500 text-white font-semibold py-2 px-6 rounded-md hover:bg-yellow-500 transition-colors"
-                              >
-                                Beginnen
-                              </Link>
-                              */}
-                        </div>
-                    </div>
                 </div>
 
-                <div
-                    className="absolute"
-                    style={{
-                        left: "calc(53% + 100px)",
-                        top: "46%",
-                        transform: "translateY(-50%)",
-                    }}
-                >
-                    <div className="text-4xl">→</div>
+                <div className="pb-36 text-4xl">→</div>
+
+                <div className=" inset-0 flex flex-col items-center">
+                    <div className="relative size-80">
+                        <svg className="size-full -rotate-90" viewBox="0 0 36 36"
+                             xmlns="http://www.w3.org/2000/svg">
+                            <circle cx="18" cy="18" r="16" fill="none" className="stroke-current text-gray-200"
+                                    strokeWidth="2"></circle>
+                            <circle cx="18" cy="18" r="16" fill="none" className="stroke-current text-red-600"
+                                    strokeWidth="2" strokeDasharray="100" strokeDashoffset={100 - 75}
+                                    strokeLinecap="round"></circle>
+                        </svg>
+                        <div className="absolute top-1/2 start-1/2 transform -translate-y-1/2 -translate-x-1/2">
+                            <span className="text-center text-4xl font-bold text-black">75%</span>
+                        </div>
+                    </div>
+
+
+                    <div className="mt-4">
+                        <h2 className="text-4xl p-4 font-k2d">Les 2</h2>
+                    </div>
+                    <div className="inline-block bg-red-600 text-white font-k2d text-xl py-2 px-24 rounded-md hover:bg-red-800 hover:text-white transition-colors"
+                    >
+                        Beginnen
+                    </div>
+                    {/*
+                            <Link
+                              to="/lessons/lesson2"
+                              className="inline-block bg-purple-500 text-white font-semibold py-2 px-6 rounded-md hover:bg-blue-600 hover:text-white transition-colors"
+                            >
+                              Beginnen
+                            </Link>
+                            */}
                 </div>
             </div>
-        </div>
-    );
+        </>
+    )
+        ;
 }
 
 export default Home;
