a7daaa51741838972e124554636dcddb0e3136e5
Lucas <sennavdkoppel@gmail.com>
2025-03-10 12:44:34 +0100

    api page css cleaned up

--- src/pages/ApiKey.jsx
+++ src/pages/ApiKey.jsx
@@ -25,12 +25,12 @@ function ApiKey() {
     return(
         <>
             <h1 className="text-black font-bold text-lg">Api Key Generator</h1>
-            <p className="text-black">
+            <p className="text-black my-4">
                 Generate de Api Key met de onderstaande knop. Zodra je op de knop klikt wordt de key aangemaakt
                 en gewhitelist. Plaats deze key in de headers van elk (fetch) request als 'apiKey': '{apiKey.key}'
             </p>
-            <button onClick={handleSubmit} className="text-black">Generate API Key</button>
-            <p className="text-black">{apiKey.key}</p>
+            <button onClick={handleSubmit} className="text-black bg-red-500 px-6 py-2 rounded-lg font-bold hover:bg-red-400">Generate API Key</button>
+            <p className="text-black font-bold my-2">API Key: {apiKey.key}</p>
         </>
     )
 }
